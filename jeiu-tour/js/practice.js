9c0a55be58e8331d9a3aab3b72fe037bade925be
sda607 <sda607@naver.com>
2022-06-20 19:35:58 +0900

    jeju-tour

---
+++ jeiu-tour/js/practice.js
@@ -0,0 +1,10 @@
+//모바일 메뉴 띄움
+$('.openMOgnb').click(function(){
+    $('header').addClass('on');
+    $('header .header_cont').slideDown('fast');
+    $('header .header_area .header_cont .closePop').show();			
+});
+$('header .header_cont .closePop').click(function(){
+    $('.header_cont').slideUp('fast');
+    $('header').removeClass('on');
+});
